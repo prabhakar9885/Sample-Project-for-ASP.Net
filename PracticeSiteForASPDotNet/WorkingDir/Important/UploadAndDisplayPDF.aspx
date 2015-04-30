8ed9c65df544fc0a1eca3f4695bfcff1f0211d5e
Prabhakar <prabhakar.9885@gmail.com>
2015-05-01 01:48:18 +0530

    Upload a PDF file and View that PDF in a HTML-DIV element.

---
+++ PracticeSiteForASPDotNet/WorkingDir/Important/UploadAndDisplayPDF.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="UploadAndDisplayPDF.aspx.cs" Inherits="PracticeSiteForASPDotNet.WorkingDir.Important.DisplayPDF" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <asp:FileUpload ID="FileUpload1" runat="server" /><br />
+    <asp:Button ID="LoadPDF" runat="server" Text="Load PDF" OnClick="LoadPDF_Click" /><br />
+    <asp:Button ID="ShowOutline" runat="server" Text="Show Outline" 
+        onclick="ShowOutline_Click" /><br />
+    <div style="margin: 5px;" id="Outline" runat="server">
+        <div id="placeholder" runat="server">
+        </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ PracticeSiteForASPDotNet/WorkingDir/Important/UploadAndDisplayPDF.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+using System.IO;
+
+namespace PracticeSiteForASPDotNet.WorkingDir.Important
+{
+    public partial class DisplayPDF : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void LoadPDF_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile &&
+                FileUpload1.FileName.EndsWith(".pdf", StringComparison.OrdinalIgnoreCase))
+            {
+
+                string path = @"/WorkingDir/Uploads/" + FileUpload1.FileName;
+                
+                if (File.Exists(Server.MapPath("~" + path)))
+                    File.Delete(Server.MapPath("~" + path));
+
+                FileUpload1.SaveAs(Server.MapPath("~" + path));
+                
+                placeholder.Attributes.Add("css", "border-color:Green; border-width: 5px");
+                placeholder.InnerHtml =
+                 @"<object data='" + path + @"' type='application/pdf' width='300' height='200'                                  runat='server' id='PdfObj'>
+                        alt : <a href='" + path + @"' runat='server' id='pdfAlt'>test.pdf</a>
+                  </object>";
+            }
+        }
+
+        protected void ShowOutline_Click(object sender, EventArgs e)
+        {
+            Outline.Attributes.Add("style", "background-color: GREEN;");
+        }
+
+    }
+}
