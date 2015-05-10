3c3e97ee240bcefae85a8519203adb5a8341297c
Prabhakar <prabhakar.9885@gmail.com>
2015-05-10 12:25:52 +0530

    Added Salary attribute to the Employee.cs

--- BusinessEntities/Employee.cs
+++ BusinessEntities/Employee.cs
@@ -10,6 +10,7 @@ namespace BusinessEntities
         public int Id { get; set; }
         public string Name { get; set; }
         public string Job { get; set; }
+        public double Salary { get; set; }
         public string ManagerName { get; set; }
     }
 }
