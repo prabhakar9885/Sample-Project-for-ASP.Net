e28a238f96d8906edeea11b87c9d9f662b059176
Prabhakar <prabhakar.9885@gmail.com>
2015-05-18 01:52:43 +0530

    Perform Edit, Update and Delete with List<Employee> as DataSource.

---
+++ TelerikWebApp1/Workspace/Telerik/RadGrid/Project/AddnewEditDelete.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AddnewEditDelete.aspx.cs"
+    Inherits="AddnewEditDelete" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <telerik:RadStyleSheetManager ID="RadStyleSheetManager1" runat="server" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <telerik:RadScriptManager ID="RadScriptManager1" runat="server">
+        <Scripts>
+            <asp:ScriptReference Assembly="Telerik.Web.UI" Name="Telerik.Web.UI.Common.Core.js" />
+            <asp:ScriptReference Assembly="Telerik.Web.UI" Name="Telerik.Web.UI.Common.jQuery.js" />
+            <asp:ScriptReference Assembly="Telerik.Web.UI" Name="Telerik.Web.UI.Common.jQueryInclude.js" />
+        </Scripts>
+    </telerik:RadScriptManager>
+    <script type="text/javascript">
+        //Put your JavaScript code here.
+    </script>
+    <telerik:RadAjaxManager ID="RadAjaxManager1" runat="server">
+    </telerik:RadAjaxManager>
+    <div>
+        <telerik:RadGrid ID="RadGrid1" runat="server" GroupPanelPosition="Top" ResolvedRenderMode="Classic"
+            OnNeedDataSource="RadGrid1_NeedDataSource1" OnUpdateCommand="RadGrid1_UpdateCommand"
+            OnInsertCommand="RadGrid1_InsertCommand" OnDeleteCommand="RadGrid1_DeleteCommand">
+            <ClientSettings>
+                <Scrolling AllowScroll="True" UseStaticHeaders="True" />
+            </ClientSettings>
+            <MasterTableView AutoGenerateColumns="False" CommandItemDisplay="Top" EditMode="InPlace"
+                DataKeyNames="Id">
+                <CommandItemSettings ShowRefreshButton="False" />
+                <Columns>
+                    <telerik:GridBoundColumn DataField="Id" DataType="System.Int32" ReadOnly="true" FilterControlAltText="Filter Id column"
+                        HeaderText="Id" UniqueName="Id">
+                    </telerik:GridBoundColumn>
+                    <telerik:GridBoundColumn DataField="Name" FilterControlAltText="Filter ename column"
+                        HeaderText="Name" UniqueName="Name">
+                    </telerik:GridBoundColumn>
+                    <telerik:GridBoundColumn DataField="ManagerName" FilterControlAltText="Filter ManagerName column"
+                        HeaderText="ManagerName" UniqueName="ManagerName">
+                    </telerik:GridBoundColumn>
+                    <telerik:GridBoundColumn DataField="Job" FilterControlAltText="Filter job column"
+                        HeaderText="Job" UniqueName="Job">
+                    </telerik:GridBoundColumn>
+                    <telerik:GridNumericColumn DataField="Salary" DataType="System.Double" DecimalDigits="2"
+                        FilterControlAltText="Filter Salary column" HeaderText="Salary" UniqueName="Salary">
+                    </telerik:GridNumericColumn>
+                    <telerik:GridEditCommandColumn HeaderText="Action" ButtonType="ImageButton">
+                    </telerik:GridEditCommandColumn>
+                    <telerik:GridButtonColumn HeaderText="Delete" ButtonType="ImageButton" CommandName="Delete">
+                    </telerik:GridButtonColumn>
+                </Columns>
+                <EditFormSettings>
+                    <EditColumn FilterControlAltText="Filter EditCommandColumn1 column" UniqueName="EditCommandColumn1">
+                    </EditColumn>
+                </EditFormSettings>
+            </MasterTableView>
+        </telerik:RadGrid>
+    </div>
+    </form>
+</body>
+</html>
---
+++ TelerikWebApp1/Workspace/Telerik/RadGrid/Project/AddnewEditDelete.aspx.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Linq;
+
+using System.Data;
+using System.Configuration;
+using System.Web.Security;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using Telerik.Web.UI;
+using DataAccessLayer;
+using System.Collections.Generic;
+using BusinessEntities;
+
+public partial class AddnewEditDelete : System.Web.UI.Page 
+{
+    EmployeeDataContract edc;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+    }
+
+
+    /// <summary>
+    /// If the data is not available in the session variable, 
+    ///             Fetch the data from the WCF Service and return it.
+    /// else,
+    ///             Return the data from the session variable.
+    /// </summary>
+    /// <returns></returns>
+    private EmployeeDataContract GetAppropriateDataObject()
+    {
+        EmployeeDataContract edc;
+        if (Session["data"] == null)
+        {
+            DataAccessService das = new DataAccessService();
+            edc = das.GetEmployeeData(1);
+            Session["data"] = edc;
+        }
+        else
+            edc = Session["data"] as EmployeeDataContract;
+        return edc;
+    }
+
+    protected void RadGrid1_NeedDataSource1(object sender, GridNeedDataSourceEventArgs e)
+    {
+        edc = GetAppropriateDataObject();
+        Session["data"] = edc;
+        ((RadGrid)sender).DataSource = edc.Employees;
+    }
+
+
+    /// <summary>
+    /// Event Handler for Update Command
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void RadGrid1_UpdateCommand(object sender, GridCommandEventArgs e)
+    {
+        if (e.Item is GridEditableItem)
+        {
+            
+            GridEditableItem gei = e.Item as GridEditableItem;
+
+            Dictionary<string, string> newValues = new Dictionary<string, string>();
+            e.Item.OwnerTableView.ExtractValuesFromItem(newValues, gei);   
+
+            int _Id = (int)gei.GetDataKeyValue("Id");
+
+            EmployeeDataContract edc = GetAppropriateDataObject();
+
+            edc.Employees = (edc.Employees.Select(emp =>
+                {
+                    if (emp.Id == _Id)
+                    {
+                        emp = new BusinessEntities.Employee
+                        {
+                            Id = _Id,
+                            Job = newValues["Job"].ToString(),
+                            ManagerName = newValues["ManagerName"].ToString(),
+                            Name = newValues["Name"].ToString(),
+                            Salary = double.Parse(newValues["Salary"].ToString())
+                        };
+                    }
+                    return emp;
+                })).ToList();
+
+            Session["data"] = edc;
+        }
+    }
+
+
+    /// <summary>
+    /// Event Hhandler for Insert Command
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void RadGrid1_InsertCommand(object sender, GridCommandEventArgs e)
+    {
+        GridEditableItem gei = e.Item as GridEditableItem;
+
+        Dictionary<string, string> newValues = new Dictionary<string, string>();
+        e.Item.OwnerTableView.ExtractValuesFromItem(newValues, gei);
+
+        edc = GetAppropriateDataObject();
+
+        edc.Employees.Add(new BusinessEntities.Employee()
+        {
+            Id = edc.Employees.Max(p => p.Id) + 1,
+            Job = newValues["Job"].ToString(),
+            ManagerName = newValues["ManagerName"].ToString(),
+            Name = newValues["Name"].ToString(),
+            Salary = double.Parse(newValues["Salary"].ToString())
+        });
+
+        Session["data"] = edc;
+    }
+
+
+
+    /// <summary>
+    /// Event handler for Delete Command
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void RadGrid1_DeleteCommand(object sender, GridCommandEventArgs e)
+    {
+        GridDataItem gdi = e.Item as GridDataItem;
+        int Id = (int)gdi.GetDataKeyValue("Id");
+
+        edc = GetAppropriateDataObject();
+
+        EmployeeDataContract temp = new EmployeeDataContract();
+        temp.Employees = new List<Employee>();
+        foreach (Employee item in edc.Employees)
+        {
+            if (item.Id == Id)
+                continue;
+            temp.Employees.Add(item);
+        }
+        edc = temp;
+        Session["data"] = edc;
+    }
+
+}
