4eadaf4c6b7eb691321673cd63f7c6c067aeda5b
Prabhakar <prabhakar.9885@gmail.com>
2015-05-20 04:40:47 +0530

    Load the RadGrid Dynamically and then perform Delete and Update operations through an SP.

---
+++ TelerikWebApp1/Workspace/Telerik/RadGrid/Project/DynamicGridWithAddEditDelete.aspx
@@ -0,0 +1,63 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DynamicGridWithAddEditDelete.aspx.cs"
+    Inherits="DynamicGridWithAddEditDelete" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <telerik:RadStyleSheetManager ID="RadStyleSheetManager1" runat="server" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <telerik:RadScriptManager ID="RadScriptManager1" runat="server">
+        <Scripts>
+            <asp:ScriptReference Assembly="Telerik.Web.UI" Name="Telerik.Web.UI.Common.Core.js" />
+            <asp:ScriptReference Assembly="Telerik.Web.UI" Name="Telerik.Web.UI.Common.jQuery.js" />
+            <asp:ScriptReference Assembly="Telerik.Web.UI" Name="Telerik.Web.UI.Common.jQueryInclude.js" />
+        </Scripts>
+    </telerik:RadScriptManager>
+    <script type="text/javascript">
+        //Put your JavaScript code here.
+    </script>
+    <telerik:RadAjaxManager ID="RadAjaxManager1" runat="server">
+    </telerik:RadAjaxManager>
+    <telerik:RadAjaxPanel ID="RadAjaxPanel1" runat="server" Height="200px" Width="300px">
+        <div>
+            <telerik:RadTextBox ID="ShipCityNamePatternTB" runat="server">
+            </telerik:RadTextBox>
+            <telerik:RadButton ID="Search" runat="server" Text="Search" OnClick="Search_Click">
+            </telerik:RadButton>
+        </div>
+        <div>
+            <telerik:RadGrid ID="RadGrid1" runat="server" AutoGenerateColumns="False" GroupPanelPosition="Top"
+                ResolvedRenderMode="Classic" OnNeedDataSource="RadGrid1_NeedDataSource" OnDeleteCommand="RadGrid1_DeleteCommand"
+                OnEditCommand="RadGrid1_EditCommand" OnInsertCommand="RadGrid1_InsertCommand"
+                OnUpdateCommand="RadGrid1_UpdateCommand">
+                <MasterTableView CommandItemDisplay="Top">
+                    <CommandItemSettings ShowRefreshButton="False" />
+                    <Columns>
+                        <telerik:GridBoundColumn DataField="OrderID" DataType="System.Int32" FilterControlAltText="Filter OrderID column"
+                            HeaderText="OrderID" ReadOnly="True" SortExpression="OrderID" UniqueName="OrderID">
+                        </telerik:GridBoundColumn>
+                        <telerik:GridBoundColumn DataField="ProductName" HeaderText="ProductName" UniqueName="ProductName">
+                        </telerik:GridBoundColumn>
+                        <telerik:GridBoundColumn DataField="ShipCity" HeaderText="ShipCity" ReadOnly="True"
+                            UniqueName="ShipCity">
+                        </telerik:GridBoundColumn>
+                        <telerik:GridEditCommandColumn HeaderText="Edit" ButtonType="ImageButton">
+                        </telerik:GridEditCommandColumn>
+                        <telerik:GridButtonColumn ButtonType="ImageButton" CommandName="Delete" FilterControlAltText="Filter column column"
+                            HeaderText="Delete" Text="Delete" UniqueName="column">
+                        </telerik:GridButtonColumn>
+                    </Columns>
+                    <EditFormSettings>
+                        <EditColumn FilterControlAltText="Filter EditCommandColumn1 column" UniqueName="EditCommandColumn1">
+                        </EditColumn>
+                    </EditFormSettings>
+                </MasterTableView>
+            </telerik:RadGrid>
+        </div>
+    </telerik:RadAjaxPanel>
+    </form>
+</body>
+</html>
---
+++ TelerikWebApp1/Workspace/Telerik/RadGrid/Project/DynamicGridWithAddEditDelete.aspx.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data;
+using System.Configuration;
+using System.Web.Security;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using Telerik.Web.UI;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+
+public partial class DynamicGridWithAddEditDelete : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Search_Click(object sender, EventArgs e)
+    {
+          //When Search button is clicked from the UI, a Postback occurs.
+          //When the Postback occues, the Page_Load event and then RadGrid1_NeedDataSource event get triggered.
+          //The RadGrid1_NeedDataSource event handler captures the text from the textbox, executes the SP and then re-initializes the RadGrid's DataSource
+    }
+
+    protected void RadGrid1_NeedDataSource(object sender, GridNeedDataSourceEventArgs e)
+    {
+        string SearchPattern = ShipCityNamePatternTB.Text.Trim();
+        DataSet ds = new DataSet();
+
+        if (SearchPattern == "")
+            return;
+
+        string ConnStr = ConfigurationManager.ConnectionStrings["NORTHWINDConnectionString2"].ToString();
+        using (SqlConnection Conn = new SqlConnection(ConnStr))
+        {
+            SqlCommand Cmd = new SqlCommand("GetOrdersWithProductInfo", Conn);
+            Cmd.CommandType = CommandType.StoredProcedure;
+            Cmd.Parameters.Add(new SqlParameter("@ShipCitySubstr", SearchPattern));
+            SqlDataAdapter Adpt = new SqlDataAdapter(Cmd);
+
+            Adpt.Fill(ds);
+        }
+
+        RadGrid1.DataSource = ds.Tables[0];
+    }
+
+    protected void RadGrid1_DeleteCommand(object sender, GridCommandEventArgs e)
+    {
+        GridDataItem Item = e.Item as GridDataItem;
+
+        int OrderId = int.Parse(Item["OrderID"].Text);
+        string ProductName = Item["ProductName"].Text;
+
+        string ConnStr = ConfigurationManager.ConnectionStrings["NORTHWINDConnectionString2"].ToString();
+        using (SqlConnection Conn = new SqlConnection(ConnStr))
+        {
+            SqlCommand Cmd = new SqlCommand("DeleteAnOrder", Conn);
+            Cmd.CommandType = CommandType.StoredProcedure;
+            Cmd.Parameters.Add(new SqlParameter("@OrderId", OrderId));
+            Cmd.Parameters.Add(new SqlParameter("@ProductName", ProductName));
+
+            Conn.Open();
+            Cmd.ExecuteNonQuery();
+            Conn.Close();
+        }
+    }
+
+    protected void RadGrid1_EditCommand(object sender, GridCommandEventArgs e)
+    {
+        GridDataItem Item = e.Item as GridDataItem;
+        Session["ProductNameOld"] = Item["ProductName"].Text;
+    }
+
+    protected void RadGrid1_InsertCommand(object sender, GridCommandEventArgs e)
+    {
+    }
+
+    protected void RadGrid1_UpdateCommand(object sender, GridCommandEventArgs e)
+    {
+        GridEditableItem Item = e.Item as GridEditableItem;
+
+        Dictionary<string, string> NewValues = new Dictionary<string, string>();
+        e.Item.OwnerTableView.ExtractValuesFromItem(NewValues, Item);
+
+        int OrderId = int.Parse(Item["OrderId"].Text);
+        string ProductNameOld = Session["ProductNameOld"].ToString();
+
+        string ConnStr = ConfigurationManager.ConnectionStrings["NORTHWINDConnectionString2"].ToString();
+        using (SqlConnection Conn = new SqlConnection(ConnStr))
+        {
+            SqlCommand Cmd = new SqlCommand("UpdateProductIdForAnOrder", Conn);
+            Cmd.CommandType = CommandType.StoredProcedure;
+            Cmd.Parameters.Add(new SqlParameter("@OrderId", OrderId));
+            Cmd.Parameters.Add(new SqlParameter("@ProductNameOld", ProductNameOld));
+            Cmd.Parameters.Add(new SqlParameter("@ProductNameNew", NewValues["ProductName"].ToString()));
+
+            Conn.Open();
+            Cmd.ExecuteNonQuery();
+            Conn.Close();
+        }
+    }
+}
+
+
+//      SP Code :
+//      =========
+
+//      CREATE PROCEDURE GetOrdersWithProductInfo 
+//      (
+//          @ShipCitySubstr		VARCHAR(20) = NULL
+//      )
+//      AS
+//          INSERT INTO	temp_GetOrdersWithProductInfo 
+//          SELECT		o.OrderID,
+//                      p.ProductName,
+//                      o.ShipCity
+//          FROM		Orders o
+//          JOIN		[Order Details] od
+//          ON			o.OrderID = od.OrderID 
+//          JOIN		Products p
+//          ON			p.ProductID = od.ProductID
+//          WHERE		o.ShipCity LIKE '%'+ISNULL(@ShipCitySubstr,'')	+'%'
+//          ORDER BY	o.OrderID
+	        
+//          SELECT		* 
+//          FROM		temp_GetOrdersWithProductInfo
+	        
+//          DELETE FROM temp_GetOrdersWithProductInfo
+//      GO
+
+//      ---------------------------------------------------------------------
+
+//      CREATE PROCEDURE UpdateProductIdForAnOrder
+//      (
+//          @ProductNameOld	VARCHAR(80),
+//          @ProductNameNew	VARCHAR(80),
+//          @OrderId		INT
+//      )
+//      AS
+//          UPDATE	[Order Details]
+//          SET		ProductID = (	SELECT	p.ProductID
+//                                  FROM	Products p
+//                                  WHERE	p.ProductName = @ProductNameNew)
+//          WHERE	OrderID = @OrderId AND
+//                  ProductID = (	SELECT	p.ProductID
+//                                  FROM	Products p
+//                                  WHERE	p.ProductName = @ProductNameOld)
+//      GO
+
+//      ---------------------------------------------------------------------
+
+//      CREATE PROCEDURE DeleteAnOrder
+//      (
+//          @ProductName	VARCHAR(80),
+//          @OrderId		INT
+//      )
+//      AS
+//      BEGIN
+//          DELETE	FROM	[Order Details]
+//          WHERE			OrderID= @OrderId AND
+//                          ProductID = ( SELECT	p.ProductID
+//                                          FROM	Products p
+//                                          WHERE	p.ProductName = @ProductName)
+
+//          DELETE	FROM	Orders
+//          WHERE			OrderID = @OrderId AND
+//                          NOT EXISTS( SELECT	1 
+//                                      FROM	[Order Details] od
+//                                      WHERE	od.OrderID=@OrderId)
+//      END
+//      GO
